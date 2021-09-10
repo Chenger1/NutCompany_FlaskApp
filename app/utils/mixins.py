0e401d67d844e2a9f791653289746aacf268566a
Chenger1 <exs2199@gmail.com>
2021-09-10 22:25:35 +0300

    AdminPermissionRequiredMixin

--- app/utils/mixins.py
+++ app/utils/mixins.py
@@ -1,4 +1,7 @@
 from flask_login import login_required
+from flask_login.utils import current_user
+
+from flask import Response
 
 
 class LoginRequiredMixin:
@@ -8,3 +11,13 @@ class LoginRequiredMixin:
     @login_required
     def dispatch_request(self, *args, **kwargs):
         return getattr(super(LoginRequiredMixin, self), 'dispatch_request')(*args, **kwargs)
+
+
+class AdminPermissionRequiredMixin:
+    """
+    Provide easy way to mark class-based views as admin permission required
+    """
+    def dispatch_request(self, *args, **kwargs):
+        if not current_user.is_admin:
+            return Response('You don`t have permissions'), 403
+        return getattr(super(AdminPermissionRequiredMixin, self), 'dispatch_request')(*args, **kwargs)
