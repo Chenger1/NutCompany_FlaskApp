77d843bb4c2f3102f1b6b51169b1799c23999bda
Chenger1 <exs2199@gmail.com>
2021-09-10 19:28:33 +0300

    init flask-login package

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 from config import config_dict
 
 db = SQLAlchemy()
 migrate = Migrate()
+login_manager = LoginManager()
 
 
 def create_app(config_name='development'):
@@ -14,6 +16,7 @@ def create_app(config_name='development'):
     config_dict[config_name].init_app(app)
     db.init_app(app)
     migrate.init_app(app, db)
+    login_manager.init_app(app)
 
     from .views.admin import main as admin_main
     from ._db import models, site_models
