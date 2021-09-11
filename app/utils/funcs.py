de0a0b4a146f86878b176852a66124f968553b94
Chenger1 <exs2199@gmail.com>
2021-09-11 21:32:54 +0300

    utils to handle files

---
+++ app/utils/funcs.py
@@ -0,0 +1,12 @@
+import os
+
+from werkzeug.utils import secure_filename
+
+from ..config import upload_folder
+
+
+def handle_files(file_data):
+    filename = secure_filename(file_data.filename)
+    save_path = os.path.join(upload_folder, filename)
+    file_data.save(save_path)
+    return save_path
