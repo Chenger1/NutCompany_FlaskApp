a7284bd8c58ac7c9b6b0649292da0994654d1ba8
Chenger1 <exs2199@gmail.com>
2021-09-11 16:08:40 +0300

    Admin users list

--- app/views/admin/users_views.py
+++ app/views/admin/users_views.py
@@ -4,11 +4,13 @@ from flask.views import MethodView
 from . import main
 
 from app.utils.mixins import LoginRequiredMixin, AdminPermissionRequiredMixin
+from app._db.models import User
 
 
 class AdminUserList(LoginRequiredMixin, AdminPermissionRequiredMixin, MethodView):
     def get(self):
-        return render_template('admin/users/admin_users.html')
+        users = User.query.filter_by(is_admin=True).all()
+        return render_template('admin/users/admin_users.html', instances=users)
 
 
 main.add_url_rule('/admin_list', view_func=AdminUserList.as_view('admin_list'))
