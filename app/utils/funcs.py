b3fd7412f9ba901bd546dd5c5665e2295af38173
Chenger1 <exs2199@gmail.com>
2021-09-13 17:52:29 +0300

    Fix for case if photo already exists

--- app/utils/funcs.py
+++ app/utils/funcs.py
@@ -6,6 +6,8 @@ from ..config import upload_folder
 
 
 def handle_files(file_data):
+    if isinstance(file_data, str):
+        return file_data
     filename = secure_filename(file_data.filename)
     save_path = os.path.join(upload_folder, filename)
     file_data.save(save_path)
