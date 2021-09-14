be212b327dc12a75c5c2c2f645d8096847d9b839
Chenger1 <exs2199@gmail.com>
2021-09-15 01:49:14 +0300

    Add SelectFieldEnum mixin to integrate Enum to WTFForms

--- app/_db/choices.py
+++ app/_db/choices.py
@@ -1,37 +1,59 @@
 from enum import Enum
 
 
-class UserTypeChoice(Enum):
+class SelectFieldEnum(Enum):
+    """
+    SelectField from WTForms requires additional preparing
+    """
+    @classmethod
+    def choices(cls):
+        """
+        SelectField requires choices either dict or tuple with (value, key) format
+        """
+        return [(choice, choice.value) for choice in cls]
+
+    @classmethod
+    def coerce(cls, item):
+        """
+        SelectField requires function to represent chosen value as valid type
+        """
+        return cls(item) if not isinstance(item, cls) else item
+
+    def __str__(self):
+        return str(self.value)
+
+
+class UserTypeChoice(SelectFieldEnum):
     legal_person = 'Юридическое лицо'
     physical_person = 'Физические лицо'
 
 
-class DeliveryTypeChoice(Enum):
+class DeliveryTypeChoice(SelectFieldEnum):
     new_mail = 'Новая почта (по Украине, оплата за счет Клиента)'
     courier = 'Куръер по Одесса'
     self_pickup = 'Самовывоз со склада'
 
 
-class PaymentChoice(Enum):
+class PaymentChoice(SelectFieldEnum):
     privat = 'LiqPay/Приват24'
     cashless = 'Безналичный расчет'
     cash = 'Наличными при получении (Наложенным платежом)'
 
 
-class OrderStatusChoice(Enum):
+class OrderStatusChoice(SelectFieldEnum):
     unpaid = 'Неоплачен'
     in_progress = 'В обработке'
     sent = 'Отправлен'
     done = 'Выполнен'
 
 
-class RequestStatusChoice(Enum):
+class RequestStatusChoice(SelectFieldEnum):
     in_progress = 'В обработке'
     closed = 'Отказано'
     done = 'Выполнен'
 
 
-class CountryChoice(Enum):
-    ua = 'Украине'
+class CountryChoice(SelectFieldEnum):
+    ua = 'Украина'
     pl = 'Польша'
     by = 'Беларусь'
