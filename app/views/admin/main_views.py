c8b9f93c93d71be3648ee047d02998cb322cd9e2
Chenger1 <exs2199@gmail.com>
2021-09-14 20:46:40 +0300

    Requests admin page

--- app/views/admin/main_views.py
+++ app/views/admin/main_views.py
@@ -2,7 +2,10 @@ from flask import render_template
 
 from . import main
 
+from app._db.models import Request
+
 from app.utils.mixins import AdminMethodView
+from app.utils.generic import ListViewMixin
 
 
 class StatisticView(AdminMethodView):
@@ -10,4 +13,10 @@ class StatisticView(AdminMethodView):
         return render_template('admin/index.html')
 
 
+class RequestView(AdminMethodView, ListViewMixin):
+    template_name = 'admin/requests.html'
+    model = Request
+
+
 main.add_url_rule('/admin', view_func=StatisticView.as_view('statistic'))
+main.add_url_rule('/admin/requests', view_func=RequestView.as_view('requests'))
