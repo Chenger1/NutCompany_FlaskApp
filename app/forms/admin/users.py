ff4389efbf4c2f27f847f15cc4bf2d1e7a233ed7
Chenger1 <exs2199@gmail.com>
2021-09-15 01:49:26 +0300

    Client pages

--- app/forms/admin/users.py
+++ app/forms/admin/users.py
@@ -1,22 +1,23 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 
-from wtforms import StringField, PasswordField, BooleanField, ValidationError, DateField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms import fields, ValidationError
+from wtforms.validators import DataRequired, Length, Email, Optional
 
 from app._db.models import User
+from app._db.choices import CountryChoice, UserTypeChoice
 
 from datetime import date
 
 
 class UserBaseForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(),
-                                             Length(1, 150),
-                                             Email()])
-    fio = StringField('FIO', validators=[DataRequired(),
-                                         Length(1, 150)])
-    phone = StringField('Phone', validators=[DataRequired(),
-                                             Length(1, 30)])
+    email = fields.StringField('Email', validators=[DataRequired(),
+                                                    Length(1, 150),
+                                                    Email()])
+    fio = fields.StringField('FIO', validators=[DataRequired(),
+                                                Length(1, 150)])
+    phone = fields.StringField('Phone', validators=[DataRequired(),
+                                                    Length(1, 30)])
     photo = FileField('Photo', validators=[FileRequired(),
                                            FileAllowed({'png', 'jpg', 'jpeg'})])
 
@@ -28,18 +29,18 @@ class UserBaseForm(FlaskForm):
 class AdminEditForm(UserBaseForm):
     photo = FileField('Photo', validators=[FileAllowed({'png', 'jpg', 'jpeg'})])
 
-    password = PasswordField('Password')
-    password2 = PasswordField('Password2')
+    password = fields.PasswordField('Password')
+    password2 = fields.PasswordField('Password2')
 
-    is_admin = BooleanField(default=True)
+    is_admin = fields.BooleanField(default=True)
 
 
 class AdminCreateForm(UserBaseForm):
-    password = PasswordField('Password', validators=[DataRequired()])
-    password2 = PasswordField('Password2', validators=[DataRequired()])
+    password = fields.PasswordField('Password', validators=[DataRequired()])
+    password2 = fields.PasswordField('Password2', validators=[DataRequired()])
 
-    is_admin = BooleanField(default=True)
-    joined = DateField('Joined')
+    is_admin = fields.BooleanField(default=True)
+    joined = fields.DateField('Joined')
 
     def validate_email(self, field):
         if User.query.filter_by(email=field.data).first():
@@ -49,3 +50,16 @@ class AdminCreateForm(UserBaseForm):
     def validate_joined(self, field):
         field.data = date.today()
         return field
+
+
+class ClientAdminPageForm(FlaskForm):
+    fio = fields.StringField('ФИО', validators=[DataRequired()])
+    email = fields.StringField('Email', validators=[Email()])
+    phone = fields.StringField('Номер телефона', validators=[DataRequired()])
+    country = fields.SelectField('Страна', choices=CountryChoice.choices(), coerce=CountryChoice.coerce)
+    city = fields.StringField('Город')
+    address = fields.StringField('Адрес')
+    index = fields.IntegerField('Индекс', validators=[Optional()])
+    type = fields.SelectField('Тип', choices=UserTypeChoice.choices(), coerce=UserTypeChoice.coerce)
+    credentials = fields.TextAreaField('Реквизиты')
+    photo = FileField('Фото', validators=[FileAllowed({'png', 'jpg', 'jpeg'})])
--- app/views/admin/users_views.py
+++ app/views/admin/users_views.py
@@ -2,8 +2,8 @@ from . import main
 
 from app.utils.mixins import AdminMethodView
 from app.utils.generic import ListViewMixin, UpdateViewMixin, CreateViewMixin, DeleteInstanceMixin
-from app._db.models import User
-from app.forms.admin.users import AdminEditForm, AdminCreateForm
+from app._db.models import User, enum_select_prepare
+from app.forms.admin.users import AdminEditForm, AdminCreateForm, ClientAdminPageForm
 
 
 class AdminUserList(AdminMethodView, ListViewMixin):
@@ -34,7 +34,37 @@ class DeleteAdminView(AdminMethodView, DeleteInstanceMixin):
     redirect_url = 'main.admin_list'
 
 
+class ClientUserList(AdminMethodView, ListViewMixin):
+    model = User
+    template_name = 'admin/users/client_users.html'
+
+    def get_queryset(self):
+        query = self.model.query.filter_by(is_admin=False)
+        return self.paginate(query)
+
+
+class ClientDetail(AdminMethodView, UpdateViewMixin):
+    model = User
+    form_class = ClientAdminPageForm
+    template_name = 'admin/users/client_detail.html'
+    redirect_url = 'main.client_list'
+
+    def get_form(self, instance=None):
+        form = super().get_form(instance)
+        return form
+
+
+class DeleteClientView(AdminMethodView, DeleteInstanceMixin):
+    model = User
+    redirect_url = 'main.client_list'
+
+
 main.add_url_rule('/admin_list', view_func=AdminUserList.as_view('admin_list'))
 main.add_url_rule('/admin_detail/<obj_id>', view_func=AdminDetail.as_view('admin_detail'))
 main.add_url_rule('/admin_detail/create_admin', view_func=CreateAdminView.as_view('create_admin'))
 main.add_url_rule('/admin_detail/<obj_id>/delete', view_func=DeleteAdminView.as_view('delete_admin'))
+
+
+main.add_url_rule('/client/list', view_func=ClientUserList.as_view('client_list'))
+main.add_url_rule('/client/detail/<obj_id>', view_func=ClientDetail.as_view('client_detail'))
+main.add_url_rule('/client/detail/<obj_id>/delete', view_func=DeleteClientView.as_view('delete_client'))
