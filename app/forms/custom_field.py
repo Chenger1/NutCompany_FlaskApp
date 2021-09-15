014e324786533104dca7047a9a8963d57688e090
Chenger1 <exs2199@gmail.com>
2021-09-15 12:33:16 +0300

    CustomFileField

--- app/forms/custom_field.py
+++ app/forms/custom_field.py
@@ -1,4 +1,7 @@
 from wtforms.fields.html5 import DateTimeLocalField
+from wtforms import FileField
+
+from app.utils.funcs import handle_files
 
 import datetime
 
@@ -14,3 +17,13 @@ class DateTimeLocalHTML5FormatField(DateTimeLocalField):
             except ValueError:
                 self.data = None
                 raise ValueError(self.gettext('Not a valid datetime value'))
+
+
+class CustomFileField(FileField):
+    """
+    Save instance to upload directory at set file path as field value
+    """
+    def process_formdata(self, valuelist):
+        if valuelist:
+            filename = handle_files(valuelist[0])
+            self.data = filename
