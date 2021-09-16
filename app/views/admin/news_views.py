dd902f757ddec3353a280c657ece1bb950c65172
Chenger1 <exs2199@gmail.com>
2021-09-16 23:30:01 +0300

    Add Detail page for news item

--- app/views/admin/news_views.py
+++ app/views/admin/news_views.py
@@ -1,5 +1,5 @@
 from app.utils.mixins import AdminMethodView
-from app.utils.generic import ListViewMixin, CreateViewMixin, UpdateViewMixin, DeleteInstanceMixin
+from app.utils.generic import ListViewMixin, CreateViewMixin, UpdateViewMixin, DeleteInstanceMixin, DetailInstanceMixin
 from app._db.site_models import NewsItem
 
 from app.forms.admin.common import NewsItemForm, NewsItemEditForm
@@ -17,14 +17,14 @@ class NewsListView(AdminMethodView, ListViewMixin):
 class CreateNewItemView(AdminMethodView, CreateViewMixin):
     model = NewsItem
     form_class = NewsItemForm
-    template_name = 'admin/news/news_detail.html'
+    template_name = 'admin/news/edit_news.html'
     redirect_url = 'main.news_list'
 
 
 class UpdateNewsItemView(AdminMethodView, UpdateViewMixin):
     model = NewsItem
     form_class = NewsItemEditForm
-    template_name = 'admin/news/news_detail.html'
+    template_name = 'admin/news/edit_news.html'
     redirect_url = 'main.news_list'
 
     def get_context(self, form, instance):
@@ -38,7 +38,13 @@ class DeleteNewsItem(AdminMethodView, DeleteInstanceMixin):
     redirect_url = 'main.news_list'
 
 
+class DetailNewsItemView(AdminMethodView, DetailInstanceMixin):
+    model = NewsItem
+    template_name = 'admin/news/news_detail.html'
+
+
 main.add_url_rule('/admin/news/list', view_func=NewsListView.as_view('news_list'))
 main.add_url_rule('/admin/news/create', view_func=CreateNewItemView.as_view('create_news'))
-main.add_url_rule('/admin/news/detail/<obj_id>', view_func=UpdateNewsItemView.as_view('edit_news'))
+main.add_url_rule('/admin/news/edit/<obj_id>', view_func=UpdateNewsItemView.as_view('edit_news'))
 main.add_url_rule('/admin/news/detail/<obj_id>/delete', view_func=DeleteNewsItem.as_view('delete_news'))
+main.add_url_rule('/admin/news/detail/<obj_id>', view_func=DetailNewsItemView.as_view('detail_news'))
