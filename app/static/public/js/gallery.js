1d5f33fa2ba98077ae3ae6a6fe8d17a4cb49ea40
Chenger1 <exs2199@gmail.com>
2021-09-18 00:16:47 +0300

    Object to get gallery images from server and render them

---
+++ app/static/public/js/gallery.js
@@ -0,0 +1,90 @@
+class GalleryManager {
+    constructor(script_root){
+        this.counter_one_items = 'counter_one_item';
+        this.counter_two_items = 'counter_two_item';
+        this.counter_three_items = 'counter_three_item';
+        this.gallery_container = $('.items_row');  // to this block new block will be appended
+
+        this.url = `${script_root}gallery`;
+    }
+
+    renderGallery(){
+        this.makeRequest(this.renderResponseItems);
+    }
+
+
+    renderResponseItems(items, $this) {
+
+        let check_iter_counter = 1;  // used to count N-iteration
+        let incrementor = 0;
+        while (incrementor <= items.length-1) {
+            if (check_iter_counter == 1) {
+                $this.renderList([items[incrementor], items[incrementor+1]], $this.counter_one_items)
+                check_iter_counter++;
+                incrementor = incrementor + 2;
+
+            }else if(check_iter_counter == 2){
+                $this.renderBlock(items[incrementor], $this.counter_two_items);
+                check_iter_counter++;
+                incrementor = incrementor + 1;
+
+            }else if(check_iter_counter == 3){
+                $this.renderList([items[incrementor], items[incrementor+1], items[incrementor+2]], $this.counter_three_items);
+                check_iter_counter = 1;
+                incrementor = incrementor + 3;
+            }
+        }
+    }
+
+    renderList(items, original_block_class){
+        for(let i = 0; i <= items.length-1; i++){
+            if(items[i]==undefined){
+                break;
+            }
+            this.renderBlock(items[i], original_block_class, i)
+        }
+    }
+
+   renderBlock(item, original_block_class, index=0){
+        let original_block = $(`.${original_block_class}`)[index]
+        let new_block = $(original_block).clone(true);
+        let block_item;
+        if(item.url != ''){
+            block_item = this.renderLink(item);
+        }else{
+            block_item = this.renderDiv(item);
+        }
+        $(new_block).removeClass(original_block_class);
+        $(new_block).find('.wrap').append(block_item);
+        
+        this.gallery_container.append(new_block);
+   }    
+
+   renderLink(item){
+    let photo_url = `/uploads/${item.photo}`;
+    let block = $(
+        `<a class="benefit__item overlay benefit__item_video_text" href="${item.url}"
+        style="background-image: url(${photo_url})">
+            <span><i class="nut-icon icons-play-icon"></i></span>
+        </a>`);
+    return block
+   }
+
+   renderDiv(item){
+    let photo_url = `/uploads/${item.photo}`;
+    let block = $(`<div class="benefit__item" style="background-image: url(${photo_url})"></div>`)
+    return block;
+   }
+
+    makeRequest(callback) {
+        let $this = this;
+
+        $.ajax({
+            url: this.url,
+            type: 'get',
+            success: function(data) {
+                return callback(data.items, $this);
+            }
+        });
+    }
+}
