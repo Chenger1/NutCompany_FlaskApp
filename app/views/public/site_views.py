a6a70d7362af34e47f29d69ae127ddd8db5e756d
Chenger1 <exs2199@gmail.com>
2021-09-18 00:17:03 +0300

    Connect gallery rendering to 'About page'

--- app/views/public/site_views.py
+++ app/views/public/site_views.py
@@ -1,9 +1,9 @@
 from . import public
 from flask.views import MethodView
-from flask import render_template
+from flask import render_template, jsonify
 
 from app._db.models import Product
-from app._db.site_models import NewsItem, AboutCompany
+from app._db.site_models import NewsItem, AboutCompany, MainPageGallery
 from app.utils.generic import DetailInstanceMixin
 
 
@@ -30,5 +30,29 @@ class AboutCompanyView(MethodView, DetailInstanceMixin):
         return context
 
 
+class GalleryView(MethodView):
+    model = MainPageGallery
+
+    def get(self, limit=None):
+        if limit:
+            instances = self.model.query.limit(int(limit))
+        else:
+            instances = self.model.query.all()
+        return jsonify({'items': self.serialize(instances)})
+
+    def serialize(self, query):
+        result = []
+        for item in query:
+            result.append({
+                'id': item.id,
+                'photo': item.photo,
+                'url': item.url
+            })
+        return result
+
+
 public.add_url_rule('/', view_func=IndexPageView.as_view('main_page'))
 public.add_url_rule('/about', view_func=AboutCompanyView.as_view('about_page'), defaults={'obj_id': 1})
+
+public.add_url_rule('/gallery', view_func=GalleryView.as_view('gallery_view'))
+public.add_url_rule('/gallery/<limit>', view_func=GalleryView.as_view('gallery_view_with_limit'))
