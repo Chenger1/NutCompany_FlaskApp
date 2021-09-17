cb2cecabb1588f0e9e56210695e40d669daf11f3
Chenger1 <exs2199@gmail.com>
2021-09-17 19:34:25 +0300

    About company page (Without gallery)

--- app/views/public/site_views.py
+++ app/views/public/site_views.py
@@ -4,6 +4,7 @@ from flask import render_template
 
 from app._db.models import Product
 from app._db.site_models import NewsItem, AboutCompany
+from app.utils.generic import DetailInstanceMixin
 
 
 class IndexPageView(MethodView):
@@ -16,4 +17,18 @@ class IndexPageView(MethodView):
         return render_template(self.template_name, products=products, news=news, about=about)
 
 
+class AboutCompanyView(MethodView, DetailInstanceMixin):
+    template_name = 'public/about.html'
+    model = AboutCompany
+
+    def get_instance(self, obj_id):
+        return self.model.query.first()
+
+    def get_context(self, **kwargs):
+        context = super().get_context(**kwargs)
+        context['news'] = NewsItem.query.limit(6)
+        return context
+
+
 public.add_url_rule('/', view_func=IndexPageView.as_view('main_page'))
+public.add_url_rule('/about', view_func=AboutCompanyView.as_view('about_page'), defaults={'obj_id': 1})
