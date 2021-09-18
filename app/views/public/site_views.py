49d260273accdd63dde7db2229b9271b25528830
Chenger1 <exs2199@gmail.com>
2021-09-18 21:51:57 +0300

    Corporate clients page

--- app/views/public/site_views.py
+++ app/views/public/site_views.py
@@ -3,8 +3,8 @@ from flask.views import MethodView
 from flask import render_template
 
 from app._db.models import Product
-from app._db.site_models import NewsItem, AboutCompany, MainPageGallery
-from app.utils.generic import DetailInstanceMixin, ListMixinApi
+from app._db.site_models import NewsItem, AboutCompany, MainPageGallery, CorporateClients
+from app.utils.generic import DetailInstanceMixin, ListMixinApi, ListViewMixin
 
 from .filters import truncate_html_filter
 
@@ -85,11 +85,18 @@ class DetailNewsItemView(MethodView, DetailInstanceMixin):
         return context
 
 
+class CorporateClientsView(MethodView, ListViewMixin):
+    model = CorporateClients
+    template_name = 'public/customers.html'
+    paginate_by = 5
+
+
 public.add_url_rule('/', view_func=IndexPageView.as_view('main_page'))
 public.add_url_rule('/about', view_func=AboutCompanyView.as_view('about_page'), defaults={'obj_id': 1})
 public.add_url_rule('/gallery_page', view_func=GalleryPageView.as_view('gallery_page'))
 public.add_url_rule('/news', view_func=NewsPageView.as_view('news_page'))
 public.add_url_rule('/news/<obj_id>', view_func=DetailNewsItemView.as_view('news_detail'))
+public.add_url_rule('/customers', view_func=CorporateClientsView.as_view('customers_page'))
 
 public.add_url_rule('/gallery', view_func=GalleryView.as_view('gallery_view'))
 public.add_url_rule('/news_api', view_func=NewsApiView.as_view('news_api_view'))
