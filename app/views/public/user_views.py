468de9ff03f389488afd38b51bdf14e206b735cf
Chenger1 <exs2199@gmail.com>
2021-09-19 00:45:21 +0300

    Add Term Of User page

--- app/views/public/user_views.py
+++ app/views/public/user_views.py
@@ -4,7 +4,7 @@ from flask.views import MethodView
 
 from app._db.models import User
 from app.forms.public.auth_forms import ClientRegistrationForm
-from app.utils.generic import CreateViewMixin
+from app.utils.generic import CreateViewMixin, TemplateMixin
 
 
 class ClientRegistrationView(MethodView, CreateViewMixin):
@@ -14,4 +14,9 @@ class ClientRegistrationView(MethodView, CreateViewMixin):
     form_class = ClientRegistrationForm
 
 
+class TermOfUserView(MethodView, TemplateMixin):
+    template_name = 'public/terms-of-use.html'
+
+
 public.add_url_rule('/registration', view_func=ClientRegistrationView.as_view('registration'))
+public.add_url_rule('/terms-of-use', view_func=TermOfUserView.as_view('term_of_use'))
